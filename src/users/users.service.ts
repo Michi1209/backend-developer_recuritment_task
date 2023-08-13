ac3eacdc2f1dd98375d6abedfa58e8a5cb062c2f
Michael Plattner <M.Plattner@student.uibk.ac.at>
2023-08-13 21:01:34 +0200

    add users entity and roles decorator, use new user entity in service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService { }
+export class UsersService { 
+    
+    private readonly users = [
+        {
+          userId: 1,
+          username: 'luigi',
+          password: 'weakpw',
+        },
+        {
+          userId: 2,
+          username: 'klaus',
+          password: 'worsepw',
+        },
+      ];
+    
+      async findOne(username: string): Promise<User | undefined> {
+        return this.users.find(user => user.username === username);
+      }
+}
