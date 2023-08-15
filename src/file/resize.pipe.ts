0fd8297ee51a787187b00c1b86782be9feef93c5
Michael Plattner <M.Plattner@student.uibk.ac.at>
2023-08-15 09:49:15 +0200

    fix pipe imports

--- src/file/resize.pipe.ts
+++ src/file/resize.pipe.ts
@@ -2,13 +2,13 @@
 https://docs.nestjs.com/pipes
 */
 
-import { PipeTransform, Injectable, ArgumentMetadata } from '@nestjs/common';
-import path from 'path';
-import sharp from 'sharp';
+import { PipeTransform, Injectable, ArgumentMetadata, BadRequestException } from '@nestjs/common';
+import * as path from 'path';
+import * as sharp from 'sharp';
 
 @Injectable()
 export class ResizeImagePipe implements PipeTransform<Express.Multer.File, Promise<string>>  {
-  async transform(image: Express.Multer.File, metadata: ArgumentMetadata): Promise<string>  {
+  async transform(image: Express.Multer.File): Promise<string>  {
     const originalName = path.parse(image.originalname).name;
     const filename = Date.now() + '-' + originalName + '.webp';
     await sharp(image.buffer)
